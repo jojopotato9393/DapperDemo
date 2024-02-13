314a65ae263e269843f670b1b9c6a8e9caeb4054
Joseph Gaughan <jojopotato9393@gmail.com>
2024-02-13 14:27:31 -0600

    test

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace DapperDemo
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System;
+using System.IO;
+           
+           
+
+namespace DapperDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //^^^^MUST HAVE USING DIRECTIVES^^^^
+
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+            var departmentRepo = new DepartmentRepo(conn);
+            departmentRepo.InsertDepartment("joes new department");
+            var departments = departmentRepo.GetAllDepartments();
+            foreach (var department in departments)
+            {
+                Console.WriteLine(department.DepartmentID);
+                Console.WriteLine(department.Name);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
         }
     }
 }
--- obj/Debug/net6.0/DapperDemo.AssemblyInfo.cs
+++ obj/Debug/net6.0/DapperDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DapperDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cdedfcaf36bf145f9fc03bff6d294170c1f446d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DapperDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DapperDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
